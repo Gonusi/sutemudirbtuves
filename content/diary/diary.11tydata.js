5174fc9e405b8596e168ea0f8a409e8184a6517f
Kasparas Anusauskas <kasparasanusauskas@gmail.com>
2026-02-03 08:02:22 +0200

    Fix URL for en

--- content/diary/diary.11tydata.js
+++ content/diary/diary.11tydata.js
@@ -1,3 +1,8 @@
 export default {
 	layout: "layouts/post.njk",
+	permalink: (data) => {
+		const stem = data.page?.filePathStem || "";
+		const cleaned = stem.replace(/\/index(\.[^/]+)?$/, "/");
+		return cleaned.endsWith("/") ? cleaned : `${cleaned}/`;
+	},
 };
